d0080f58bf3d464b38d09f3b8cb71d0edcf10ada
Sourabh <sourabhkhairwal@gmail.com>
2025-07-15 04:50:09 +0530

    Create index.js

---
+++ frontend/src/components/forms/index.js
@@ -0,0 +1,7 @@
+export { default as PersonalInfoForm } from './PersonalInfoForm';
+export { default as SkillsForm } from './SkillsForm';
+export { default as EducationForm } from './EducationForm';
+export { default as ExperienceForm } from './ExperienceForm';
+export { default as ProjectsForm } from './ProjectsForm';
+export { default as BlogsForm } from './BlogsForm';
+export { default as ContactForm } from './ContactForm';
